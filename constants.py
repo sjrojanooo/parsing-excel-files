5163835e97b331cdf6e4c2bc9127485560ecf16d
sjrojanooo <sjrojano@gmail.com>
2022-12-28 13:03:29 -0700

    function to identify report date. Created a constants path to hold all of my constant variables

---
+++ constants.py
@@ -0,0 +1,8 @@
+# constants
+# commodity list 
+commodity_list = ['Cauliflower', 'Cauliflower Organic', 'Wrap', 'Wrap Organic','Naked Flat Pack Lettuce',
+       'Romaine Hearts','Romaine Hearts Organic','Romaine Flat Pack','Romaine Flat Pack Organic',
+       'Romaine Cellos','Mix Leaf','Mix Leaf Organic','Mix Leaf Cellos','Broccoli','Broccoli Organic',
+       'Mix Vegetables Rates','Broccoli Bulk Organic']
+# report file path
+updated_report_path = './data/updated-cooler-counts/weekly-validation.xlsx'
