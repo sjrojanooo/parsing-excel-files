92aac16193ca369f9abe633a0baab8508f468952
sjrojanooo <sjrojano@gmail.com>
2022-12-28 18:16:45 -0700

    generated indexes for commodity titles. Need to make a function to clean the code up. Next will be generating the validation report.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from pandas import DataFrame
 import os
 import re
 import importlib
@@ -6,7 +7,7 @@ import importlib
 constants = importlib.import_module('constants')
 
 # locate the date reported anywhere on the page, in case of any format changes. 
-def capture_report_date(updated_report_df):
+def capture_report_date(updated_report_df: DataFrame):
     report_date = None
     for i in range(0, len(updated_report_df)): 
         for column in updated_report_df.columns: 
@@ -18,18 +19,38 @@ def capture_report_date(updated_report_df):
                     break
     return report_date
 
-def find_titles(): 
-    pass
+# will capture commodity titles at each index level anywhere on the excel file. 
+# return tuples/set of commodity and index
+def identify_commodity_title_locations(updated_report_df: DataFrame, commodity_list: list):
+    commodity_and_index_set = []; 
+    for i in range(0, len(updated_report_df)):
+        for column in updated_report_df.columns: 
+            commodity = str(updated_report_df.loc[i, column]).strip()
+            if commodity in commodity_list: 
+                commodity_captured_set = (i, commodity) 
+                commodity_and_index_set.append(commodity_captured_set) 
+    return commodity_and_index_set
 
 def main(): 
-    updated_report_file_path = constants.updated_report_path
-    updated_report = pd.read_excel(updated_report_file_path)
-    report_date = capture_report_date(updated_report)
-    print(report_date)
-    for i in range(0, len(updated_report)):
-        for column in updated_report.columns: 
-            value = str(updated_report.loc[i, column]).strip()
-            print(value)
+    updated_report_path = constants.updated_report_path
+    commodity_list = constants.commodity_list
 
+    updated_report = pd.read_excel(updated_report_path)
+    report_date = capture_report_date(updated_report)
+    commodity_index_list = identify_commodity_title_locations(updated_report, commodity_list)
+
+    # make function
+    product_df = DataFrame(commodity_index_list)
+    test_list = []; 
+    for i in range(0,len(product_df) -1):
+        for x in range(int(product_df.loc[i, 0]), int(product_df.loc[i+1, 0])):
+            test_list.append((x, str(product_df.loc[i, 1])))
+            if x == product_df.loc[i+1, 0]:
+                continue
+
+    grouping_df = DataFrame(test_list)
+    final_df = updated_report.join(grouping_df.set_index(0))
+    final_df['report_date'] = report_date
+    print(final_df)
 if __name__ == '__main__':
     main()
