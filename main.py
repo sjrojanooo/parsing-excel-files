bf52c556579e69912905a4cbcb19a44d27228005
sjrojanooo <sjrojano@gmail.com>
2022-12-29 09:16:51 -0700

    updates

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ def main():
     commodity_list = constants.commodity_list
     updated_report = read_excel(updated_report_path)
     report_date = report_transformations.capture_report_date(updated_report)
-    commodity_index_list = report_transformations.identify_commodity_title_locations(updated_report, commodity_list)
-    product_df = DataFrame(commodity_index_list)
+    product_df = report_transformations.identify_commodity_title_locations(updated_report, commodity_list)
     grouping_df = report_transformations.generate_index_dataframe(product_df)
     final_df = report_transformations.transform_final_df(updated_report, grouping_df, report_date) 
     final_df = report_transformations.filter_out_bad_values(final_df)
