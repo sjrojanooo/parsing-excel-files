f639e815e34f536cbe1f5843e64207d829700981
sjrojanooo <sjrojano@gmail.com>
2022-12-28 23:10:32 -0700

    writing out final file, wil provide next steps tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from pandas import DataFrame
-import os
 import re
 import importlib
 
@@ -31,26 +30,49 @@ def identify_commodity_title_locations(updated_report_df: DataFrame, commodity_l
                 commodity_and_index_set.append(commodity_captured_set) 
     return commodity_and_index_set
 
+# capture indexes between next commodity
+def generate_index_dataframe(product_df: DataFrame) -> DataFrame:
+    test_list = []; 
+    for i in range(0,len(product_df) -1):
+        for x in range(int(product_df.loc[i, 0]), int(product_df.loc[i+1, 0]) -1):
+            test_list.append((x, str(product_df.loc[i, 1])))
+    grouping_df = DataFrame(test_list)
+    return grouping_df
+
+def transform_final_df(updated_report: DataFrame, grouping_df: DataFrame, report_date: str) -> DataFrame: 
+    final_df = updated_report.join(grouping_df.set_index(0))
+    final_df['report_date'] = report_date
+    return final_df
+
+def filter_out_bad_values(input_df: DataFrame) -> DataFrame: 
+    # rename the first column for a default values
+    output_df = input_df.dropna(subset=[input_df.columns[0], input_df.columns[1], input_df.columns[2]])
+    output_df = output_df[[output_df.columns[0], output_df.columns[1],output_df.columns[7], output_df.columns[8]]]
+    output_df = output_df[output_df[output_df.columns[0]].isin(['Commodities']) == False]
+    output_df = output_df.loc[output_df[output_df.columns[0]].str[:5] != 'Total']
+    return output_df 
+
+def rename_columns(input_df: DataFrame) -> DataFrame:
+    output_df = input_df.rename(columns={input_df.columns[0]:'description', 
+                                        input_df.columns[1]: 'packed', 
+                                        input_df.columns[2]: 'commodity'})
+    output_df = output_df[['report_date', 'commodity', 'description', 'packed']]
+    return output_df
+
+def write_out_clean_file(input_df: DataFrame) -> None: 
+    input_df.to_excel('./data/cleaned-report/clean-updated-counts.xlsx', index=False)
+
 def main(): 
     updated_report_path = constants.updated_report_path
     commodity_list = constants.commodity_list
-
     updated_report = pd.read_excel(updated_report_path)
     report_date = capture_report_date(updated_report)
     commodity_index_list = identify_commodity_title_locations(updated_report, commodity_list)
-
-    # make function
     product_df = DataFrame(commodity_index_list)
-    test_list = []; 
-    for i in range(0,len(product_df) -1):
-        for x in range(int(product_df.loc[i, 0]), int(product_df.loc[i+1, 0])):
-            test_list.append((x, str(product_df.loc[i, 1])))
-            if x == product_df.loc[i+1, 0]:
-                continue
-
-    grouping_df = DataFrame(test_list)
-    final_df = updated_report.join(grouping_df.set_index(0))
-    final_df['report_date'] = report_date
-    print(final_df)
+    grouping_df = generate_index_dataframe(product_df)
+    final_df = transform_final_df(updated_report, grouping_df, report_date) 
+    final_df = filter_out_bad_values(final_df)
+    final_df = rename_columns(final_df)
+    write_out_clean_file(final_df)
 if __name__ == '__main__':
     main()
